864f199614f21891489115faf0dbb6a9b262d25b
Dylan Graham <dylan.graham@gmail.com>
2017-01-06 16:11:54 +1100

    contacts

--- lib/contact_data.dart
+++ lib/contact_data.dart
@@ -4,3 +4,14 @@ class Contact {
 
   const Contact({this.fullName, this.email});
 }
+
+const kContacts = const <Contact>[
+    const Contact(
+      fullName: 'Romain Hoogmoed',
+      email:'romain.hoogmoed@example.com'
+    ),
+    const Contact(
+      fullName: 'Emilie Olsen',
+      email:'emilie.olsen@example.com'
+    )
+];
