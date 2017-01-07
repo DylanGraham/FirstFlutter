350aac8d5f98b30307e0087e0c01cbb47088bda8
Dylan Graham <dylan.graham@gmail.com>
2017-01-08 10:30:40 +1100

    contact list

--- lib/contact_view.dart
+++ lib/contact_view.dart
@@ -1,19 +1,49 @@
 import 'package:flutter/material.dart';
+import 'contact_data.dart';
 
-class _ContactListItem extends StatelessWidget {
-  final Contact _contact;
+class _ContactListItem extends ListItem {
 
-  _ContactListItem(this._contact);
+  _ContactListItem(Contact contact) :
+        super(
+          title: new Text(contact.fullName),
+          subtitle: new Text(contact.email),
+          leading: new CircleAvatar(
+              child: new Text(contact.fullName[0])
+          )
+      );
+
+}
+
+class ContactList extends StatelessWidget {
+
+  final List<Contact> _contacts;
+
+  ContactList(this._contacts);
 
   @override
   Widget build(BuildContext context) {
-    return new ListItem(
-        leading: new CircleAvatar(
-            child: new Text(_contact.fullName[0])
-        ),
-        title: new Text(_contact.fullName),
-        subtitle: new Text(_contact.email)
+    return new MaterialList(
+        type: MaterialListType.twoLine,
+        padding: new EdgeInsets.symmetric(vertical: 8.0),
+        children: _buildContactList()
     );
   }
 
+  List<_ContactListItem> _buildContactList() {
+    return _contacts.map((contact) => new _ContactListItem(contact))
+        .toList();
+  }
+}
+
+class ContactsPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Contacts"),
+        ),
+        body: new ContactList(kContacts)
+    );
+  }
 }
